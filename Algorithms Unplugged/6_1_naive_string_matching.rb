632f84c99c7aa4a32ca9d237c8ca20a01d645958
heshamamohamed <heshamamohamed323@gmail.com>
2022-03-27 10:52:26 +0200

    fixed a bug where it always skipped matching the first letter

--- Algorithms Unplugged/6_1_naive_string_matching.rb
+++ Algorithms Unplugged/6_1_naive_string_matching.rb
@@ -4,8 +4,8 @@ def naive_search(text, string)
   # debugger
   text_pos = 0 # text iterator
   while text_pos <= text.length - string.length + 1
-    string_pos = string.length # string iterator
-    while string_pos >= 0 && string[string_pos] == text[text_pos + string_pos - 1]
+    string_pos = string.length - 1 # string iterator
+    while string_pos >= 0 && string[string_pos] == text[text_pos + string_pos]
       string_pos -= 1
       puts "Occurence at position #{text_pos}" if string_pos.zero? # iterated over all string
     end
