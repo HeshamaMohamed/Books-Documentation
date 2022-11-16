be8b366b8af41555c19745a9e43276ba05b40f26
hesham <heshamamohamed323@gmail.com>
2022-11-16 15:58:39 +0200

    Fundamentals of React: Meet another component

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -25,26 +25,40 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
+      <Search />
 
       <hr />
-      
-      <ul>
-        {list.map((item) => 
-          <li key={item.objectID}> 
-            <span>
-              <a href={item.url}>{item.title} </a>
-            </span>
-            <span>{item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.author} </span>
-          </li>
-          )
-        }
-      </ul>
+
+      <List/>
     </div>
-  )
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) =>
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title} </a>
+          </span>
+          <span>{item.author} </span>
+          <span>{item.num_comments} </span>
+          <span>{item.author} </span>
+        </li>
+      )
+      }
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+    </>
+  );
 }
 
 export default App
