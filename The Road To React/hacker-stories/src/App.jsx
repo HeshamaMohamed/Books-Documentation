e23015e5748d8c4fa09829135f6b735d12c3104f
hesham <heshamamohamed323@gmail.com>
2022-11-16 15:24:26 +0200

    Fundamentals of React: React JSX

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-const title = 'React';
+const welcome = {title: 'React', greeting: 'Hey'}
+function getTitle(title){
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>{getTitle('Hello React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   )
 }
