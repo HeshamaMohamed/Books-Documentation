f4a06ea3548d5cfe7f071cb7d7459351307655a6
hesham <heshamamohamed323@gmail.com>
2022-11-16 15:08:13 +0200

    Fundamentals of React: Meet the React component

---
+++ The Road To React/hacker-stories/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const title = 'React';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
