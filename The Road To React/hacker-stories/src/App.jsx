66bd33764eadb031c28784de9b6ee198afd5a1b6
hesham <heshamamohamed323@gmail.com>
2022-11-16 15:47:22 +0200

    Fundamentals of React: Lists in React

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -1,20 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-const welcome = {title: 'React', greeting: 'Hey'}
-function getTitle(title){
-  return title;
-}
+import * as React from 'react'
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
-      <h1>{getTitle('Hello React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
+
+      <hr />
+      
+      <ul>
+        {list.map((item) => 
+          <li key={item.objectID}> 
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.author} </span>
+          </li>
+          )
+        }
+      </ul>
     </div>
   )
 }
