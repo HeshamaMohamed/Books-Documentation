e26aaf6203c1f3384d276af43e6c79d2b77abd9b
hesham <heshamamohamed323@gmail.com>
2022-11-26 13:45:08 +0200

    Fundamentals of React: React props

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -1,26 +1,28 @@
 
 import * as React from 'react'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+
 
 const App = () =>{
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -29,28 +31,31 @@ const App = () =>{
 
       <hr />
 
-      <List/>
+      <List list={stories}/>
     </div>
   );
 }
-  
 
-const List = () =>
+
+const List = (props) =>
   (
     <ul>
-      {list.map((item) =>
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title} </a>
-          </span>
-          <span>{item.author} </span>
-          <span>{item.num_comments} </span>
-          <span>{item.author} </span>
-        </li>
+      {props.list.map((item) =>
+        <Item item={item}/>
       )}
     </ul>
   );
 
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    <span>
+      <a href={props.item.url}>{props.item.title} </a>
+    </span>
+    <span>{props.item.author} </span>
+    <span>{props.item.num_comments} </span>
+    <span>{props.item.author} </span>
+  </li>
+);
 
 const Search = () => {
   const handleChange = (event) =>{
@@ -67,6 +72,6 @@ const Search = () => {
     </>
   );
 }
-  
+
 
 export default App
