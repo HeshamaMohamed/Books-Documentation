5cf7f3eaf51570faea21c5c6dc14ed5332512599
hesham <heshamamohamed323@gmail.com>
2022-12-02 16:10:22 +0200

    Fundamentals of React: React Custom Hooks

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -1,7 +1,17 @@
 
 import * as React from 'react'
 
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue]
+}
 
 const App = () =>{
   const stories = [
@@ -23,13 +33,7 @@ const App = () =>{
     }
   ]
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'React'
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
