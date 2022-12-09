0e7076790a907792fca7370c4f633e20b5795694
hesham <heshamamohamed323@gmail.com>
2022-12-09 16:01:36 +0200

    Fundamentals of React: React Asynchronous Data

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -32,10 +32,21 @@ const initialStories = [
   }
 ]
 
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+  );
+
 const App = () =>{
 
   const [searchTerm, setSearchTerm] = useStorageState('search', '');
-  const [stories, setStories] = React.useState(initialStories)
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) =>{
     const newStories = stories.filter(story => item.objectID !== story.objectID );
