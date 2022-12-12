75258b5b6be40af3b95afa7ad4ca5f0752043fd9
hesham <heshamamohamed323@gmail.com>
2022-12-12 07:15:29 +0200

    Fundamentals of React: React Advanced State

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -37,10 +37,25 @@ const getAsyncStories = () =>
     setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
   );
 
+  const storiesReducer = (state, action) => {
+    switch(action.type) {
+      case 'SET_STORIES':
+        return action.payload;
+      case 'REMOVE_STORY':
+        return state.filter(story => action.payload.objectID !== story.objectID);
+      default:
+        throw new Error();
+    }
+  };
+
 const App = () =>{
 
   const [searchTerm, setSearchTerm] = useStorageState('search', '');
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -48,16 +63,20 @@ const App = () =>{
     setIsLoading(true);
 
     getAsyncStories().then(result => {
-      setStories(result.data.stories);
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories,
+      });
       setIsLoading(false);
     })
     .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) =>{
-    const newStories = stories.filter(story => item.objectID !== story.objectID );
-
-    setStories(newStories);
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
   };
 
   const handleSearch = (event) => {
