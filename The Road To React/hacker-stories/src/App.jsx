be22cac3989dc1109d1026c4b0e54d7bc2b453a6
hesham <heshamamohamed323@gmail.com>
2022-12-15 09:24:57 +0200

    Fundamentals of React: Third-Party Libraries in React

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -1,5 +1,6 @@
 
 import * as React from 'react'
+import axios from 'axios'
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -80,12 +81,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
