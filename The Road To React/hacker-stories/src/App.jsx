89667147b9320cb699ab8aaa7f935f56f1e45274
hesham <heshamamohamed323@gmail.com>
2022-12-15 08:36:26 +0200

    Fundamentals of React: Data Re-Fetching in React

--- The Road To React/hacker-stories/src/App.jsx
+++ The Road To React/hacker-stories/src/App.jsx
@@ -76,9 +76,11 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    if (searchTerm === '') return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then( (response) => response.json() )
       .then(result => {
         dispatchStories({
@@ -89,7 +91,7 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -101,10 +103,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
 
   return (
     <div>
@@ -127,7 +125,7 @@ const App = () => {
         <p>Loading...</p>
       ) : (
         <List
-          list={searchedStories}
+          list={stories.data}
           onRemoveItem={handleRemoveStory}
         />
       )}
